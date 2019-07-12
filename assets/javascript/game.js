f242f7af5fa494414af90fc0c94db5efc17f2c5e
Esteban Morell <estebanmorellb@gmail.com>
2019-07-12 02:50:07 -0500

    working in showing characters

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  var attacknumbers= 0;
+  var yourhp= 0;
+  var defenderhp= 0;
+  $("#characters").empty();
+
+  var characters =[
+    {
+      name: "Harry",
+      srcrt:"assets/images/1.jpg",
+      hpoints: 130,
+      apower:10,
+      capower:30,
+    },{
+      name:"Hermione",
+      srcrt:"assets/images/4.jpg",
+      hpoints:110,
+      apower:8,
+      capower:22,
+    },{
+      name:"Malfoy",
+      srcrt:"assets/images/3.jpg",
+      hpoints:110,
+      apower:6,
+      capower:20,
+    },{
+      name:"Snape",
+      srcrt:"assets/images/5.jpg",
+      hpoints:100,
+      apower:8,
+      capower:24,
+    },{
+      name:"Voldemort",
+      srcrt:"assets/images/2.jpg",
+      hpoints:150,
+      apower:9,
+      capower:25,
+    }
+  ];
+
+  /////creating dinamicaly characters
+
+
+  
+
+});
